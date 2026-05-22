7bbe56b75b637c456d0434d0c6713e09b5f41a12
moelsaeed80-ai <moelsaeed80@gmail.com>
2026-05-22 22:03:53 +0300

    Add additional input features for prediction

--- app.py
+++ app.py
@@ -23,23 +23,49 @@ num_dependents = st.number_input("Number of Dependents")
 
 if st.button("Predict"):
 
-    input_data = pd.DataFrame([[
-        duration,
-        credit_amount,
-        installment_commitment,
-        residence_since,
-        age,
-        existing_credits,
-        num_dependents
-    ]], columns=[
-        "duration",
-        "credit_amount",
-        "installment_commitment",
-        "residence_since",
-        "age",
-        "existing_credits",
-        "num_dependents"
-    ])
+   input_data = pd.DataFrame([[
+    checking_status,
+    duration,
+    credit_history,
+    purpose,
+    credit_amount,
+    savings_status,
+    employment,
+    installment_commitment,
+    personal_status,
+    other_parties,
+    residence_since,
+    property_magnitude,
+    age,
+    other_payment_plans,
+    housing,
+    existing_credits,
+    job,
+    num_dependents,
+    own_telephone,
+    foreign_worker
+]], columns=[
+    "checking_status",
+    "duration",
+    "credit_history",
+    "purpose",
+    "credit_amount",
+    "savings_status",
+    "employment",
+    "installment_commitment",
+    "personal_status",
+    "other_parties",
+    "residence_since",
+    "property_magnitude",
+    "age",
+    "other_payment_plans",
+    "housing",
+    "existing_credits",
+    "job",
+    "num_dependents",
+    "own_telephone",
+    "foreign_worker"
+])   
 
     input_scaled = scaler.transform(input_data)
 
